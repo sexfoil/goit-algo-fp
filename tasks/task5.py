d0f016ea6a8652c3d2646365ee914bccab2d9657
Viacheslav_Poliakov <Viacheslav_Poliakov@epam.com>
2025-02-17 17:23:07 +0200

    Edit code

--- tasks/task5.py
+++ tasks/task5.py
@@ -20,14 +20,14 @@ def add_edges(graph, node, pos, x=0, y=0, layer=1):
 		graph.add_node(node.id, color=node.color, label=node.val) # Використання id та збереження значення вузла
 	if node.left:
 		graph.add_edge(node.id, node.left.id)
-		l = x - 1 / 2 ** layer
-		pos[node.left.id] = (l, y - 1)
-		l = add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
+		left = x - 1 / 2 ** layer
+		pos[node.left.id] = (left, y - 1)
+		left = add_edges(graph, node.left, pos, x=left, y=y - 1, layer=layer + 1)
 	if node.right:
 		graph.add_edge(node.id, node.right.id)
-		r = x + 1 / 2 ** layer
-		pos[node.right.id] = (r, y - 1)
-		r = add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+		right = x + 1 / 2 ** layer
+		pos[node.right.id] = (right, y - 1)
+		right = add_edges(graph, node.right, pos, x=right, y=y - 1, layer=layer + 1)
 	return graph
 
 def draw_tree(tree_root):
